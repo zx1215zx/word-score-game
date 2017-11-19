463909461f95ed217ec4b841640ff251677d528f
zx1215zx <zx_1215_zx@126.com>
2017-11-19 14:07:03 +0800

    word scord game

--- js/word-score-game.js
+++ js/word-score-game.js
@@ -103,59 +103,71 @@ var BAG_OF_LETTERS = [
 
 var YOUR_HAND = new Array();
 var SCORE = 0;
+var HIGH_SCORE=0;
 function startGame() {
 	addNumbersFromBag();
 	displayHand();
-	
 };
-
-
+function endGame(){
+	alert("Game over.Your score is: "+SCORE);
+}
+function startNewGame(){
+	clearClasses();
+	//init parameters
+	SCORE=0;
+	randomLetter=BAG_OF_LETTERS;
+	allLetter=BAG_OF_LETTERS;
+	YOUR_HAND = new Array();
+	startGame();
+}
 
 function addNumbersFromBag(){
-	console.log("your hand has:" + YOUR_HAND.length);
-	for(i=YOUR_HAND.length; i < 7; i++){
+	//console.log("your hand has:" + YOUR_HAND.length);
+
+	for(var i=YOUR_HAND.length; i < 7; i++){
 		YOUR_HAND[i] = getAvailableLetter();
 	}
-	
 }
 
 
 function displayHand(){
-	console.log("your hand has:" + YOUR_HAND.length);
-	for (i = 0; i < YOUR_HAND.length; i++) {
-
-		console.log("#letter-" + (i+1) +" set to " + YOUR_HAND[i].letter);
-		$( "#letter-" + (i+1)).addClass("letter-" + YOUR_HAND[i].letter);
-		$( "#points-" + (i+1)).addClass("points-" + YOUR_HAND[i].pointsWhenLettersUsed);
-		
-		
-		
-		
-		$( "#letter-" + (i+1)).html(YOUR_HAND[i].letter);
-		
-		$( "#points-" + (i+1)).html(YOUR_HAND[i].pointsWhenLettersUsed);
+	if(allLetter.length<= YOUR_HAND.length){
+		for(var i = 0; i < allLetter.length; i++){
+			addClasses(i);
+		}
+		return;
 	}
+	for (var i = 0; i < YOUR_HAND.length; i++){
+		addClasses(i);
+	}
+}
+function addClasses(index) {
+	$( "#letter-" + (index+1)).addClass("letter-" + YOUR_HAND[index].letter);
+	$( "#points-" + (index+1)).addClass("points-" + YOUR_HAND[index].pointsWhenLettersUsed);
 	
+	$( "#letter-" + (index+1)).html(YOUR_HAND[index].letter);
+	$( "#points-" + (index+1)).html(YOUR_HAND[index].pointsWhenLettersUsed);
 }
 
 
-
+var allLetter=BAG_OF_LETTERS;
+var randomLetter=BAG_OF_LETTERS;
 function getAvailableLetter(){
 	var randomIndex = Math.floor(Math.random() * BAG_OF_LETTERS.length);
-	var randomLetter = BAG_OF_LETTERS.splice(randomIndex, 1);
-	console.log(randomLetter[0]);
+    randomLetter =  allLetter.splice(randomIndex, 1);
+	//console.log(randomLetter[0]);
 	return randomLetter[0];
 }
 
 
 function findWordToUse(){
  //TODO Your job starts here.
-	alert("Your code needs to go here");	
+	alert("Your code needs to go here");
 }
 function humanFindWordToUse(){
-	
+
 	 var humanFoundWord = $( "#human-word-input").val();
-	 console.log("Checking human workd of:" + humanFoundWord);
+	// console.log("Checking human workd of:" + humanFoundWord);
 	 if(isThisAWord(humanFoundWord)){
 		 if(haveLettersForWord(humanFoundWord)){
 			 successfullyAddedWord(humanFoundWord);
@@ -165,7 +177,7 @@ function humanFindWordToUse(){
 	 }else{
 		 alert(humanFoundWord + " is not a valid word.");
 	 }
-		
+
 }
 
 
@@ -176,25 +188,33 @@ function successfullyAddedWord(foundWord){
 	addNumbersFromBag();
 	displayHand();
 	$( "#human-word-input").val('');
-	
+
 }
 
 function addToScore(letterToAddToScore){
 	SCORE = SCORE + letterToAddToScore.pointsWhenLettersUsed;
-	console.log(letterToAddToScore.pointsWhenLettersUsed + "<-Points added for " + letterToAddToScore.letter);
+
+	//console.log(letterToAddToScore.pointsWhenLettersUsed + "<-Points added for " + letterToAddToScore.letter);
 	$( "#score-number").html(SCORE);
 }
 
+function addToHighScore(){
+	if(SCORE>=HIGH_SCORE){
+		HIGH_SCORE=SCORE;
+		$( "#highScore-number").html(HIGH_SCORE);
+	}
+	/*$("#highScore-number").data("highScore",HIGH_SCORE);*/
+}
 
 function takeOutUsedLetters(){
-	
-	for(ii=0; ii < YOUR_HAND.length; ii++){
+
+	for(var ii=0; ii < YOUR_HAND.length; ii++){
 		if(YOUR_HAND[ii].used){
 			addToScore(YOUR_HAND[ii]);
 			YOUR_HAND.splice(ii, 1);
 			ii = ii-1;
 		}else{
-			console.log(YOUR_HAND[ii].letter + "<- Not Used");
+			//console.log(YOUR_HAND[ii].letter + "<- Not Used");
 		}
 	}
 	
@@ -203,14 +223,14 @@ function takeOutUsedLetters(){
 function haveLettersForWord(aProposedWord){
 	//You could code the _ logic could go in this function
 	var wordAsArray = aProposedWord.toUpperCase().split("");
-	for (i = 0; i < wordAsArray.length; i++) {
+	for (var i = 0; i < wordAsArray.length; i++) {
 		var foundLetter = false;
-		console.log(wordAsArray[i] + "<-For match");
-		for(ii=0; ii<YOUR_HAND.length; ii++){
-			console.log("              " + YOUR_HAND[ii].letter + "<-Checking");
+		//console.log(wordAsArray[i] + "<-For match");
+		for(var ii=0; ii<YOUR_HAND.length; ii++){
+			//console.log("              " + YOUR_HAND[ii].letter + "<-Checking");
 			if(YOUR_HAND[ii].letter == wordAsArray [i]){
 				if(!YOUR_HAND[ii].used && !foundLetter){
-					console.log("     " + YOUR_HAND[ii].letter + "<-Found");
+				//	console.log("     " + YOUR_HAND[ii].letter + "<-Found");
 					YOUR_HAND[ii].used = true;
 					foundLetter = true;
 					
@@ -230,8 +250,7 @@ function haveLettersForWord(aProposedWord){
 
 
 function resetHand(){
-	
-	for(ii=0; ii<YOUR_HAND.length; ii++){
+	for(var ii=0; ii<YOUR_HAND.length; ii++){
 		YOUR_HAND[i].used = false;
 	}
 }
@@ -245,21 +264,40 @@ function isThisAWord(aProposedWord){
 
 function retireHand(){
 	//Loose all the points in your hand
+	if(randomLetter.length==0){
+		//get high_score
+		addToHighScore();
+		//game over
+		endGame();
+		return;
+	}
 	clearClasses();
 	YOUR_HAND = new Array();
 	addNumbersFromBag();
 	displayHand();
+
 }
 function clearClasses(){
-	for(ii=0; ii < YOUR_HAND.length; ii++){
+	if(allLetter.length<= YOUR_HAND.length){
+		for(var i = 0; i < allLetter.length; i++){
+			removeClasses(i);
+		}
+		return;
+	}
+	for(var ii=0; ii < YOUR_HAND.length; ii++){
+		removeClasses(ii);
+	}
+	function removeClasses(ii){
 		$("#letter-" + (ii+1)).removeClass("letter-" + YOUR_HAND[ii].letter);
 		$("#points-" + (ii+1)).removeClass("points-" + YOUR_HAND[ii].pointsWhenLettersUsed);
 	}
 }
 
+
+
 $(document).ready(function() {
 	startGame();
-	
+
 	$("#find-word-button").click(function() {
 		findWordToUse();
 	});
@@ -274,4 +312,12 @@ $(document).ready(function() {
 			humanFindWordToUse();
 		}
 	});
+	//jQuery
+	//$('#score-box').bind("click",".new_game_button",startNewGame());
+	$("#new_game_button").click(function() {
+		startNewGame();
+	});
+
+	/*var highScore=$("#highScore-number").data("highScore");
+	$( "#highScore-number").html(highScore);*/
 });
